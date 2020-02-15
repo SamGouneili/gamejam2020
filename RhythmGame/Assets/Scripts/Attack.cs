363630281333c0db600d1491b3690232610fc99f
Alex Brown <43386112+ajwb@users.noreply.github.com>
2020-02-15 10:04:59 -0800

    Remade meta files for package thigns that were giving errors and added attack scripts

---
+++ RhythmGame/Assets/Scripts/Attack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public enum AttackDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
