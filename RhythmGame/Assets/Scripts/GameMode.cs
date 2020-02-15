b40012d84f94a1f2d4742eee2c8cc9791abcf9c3
Alex Brown <43386112+ajwb@users.noreply.github.com>
2020-02-14 22:43:43 -0800

    Main Menu button goes to battle 1

--- RhythmGame/Assets/Scripts/GameMode.cs
+++ RhythmGame/Assets/Scripts/GameMode.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameMode : MonoBehaviour
 {
+    //BattleState Battle = GetComponent<BattleState>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Game Begin");
+        LoadMainMenu();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void BattleStart(BattleState BattleToStart) {
+        print("Begin Battle");
+    }
+
+    void LoadMainMenu() {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    void OnBattleVictory() {
+        print("Victory");
+    }
+
+    void OnBattleLoss() {
+        print("Loss");
     }
 }
---
+++ RhythmGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	public Button yourButton;
+
+	void Start()
+	{
+		yourButton.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+		SceneManager.LoadScene("Battle1", LoadSceneMode.Single);
+	}
+}
