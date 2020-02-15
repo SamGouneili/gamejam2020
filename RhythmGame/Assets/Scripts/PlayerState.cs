8eb7d42fe4a41611f0315e0b56155415ecba2083
Alex Brown <43386112+ajwb@users.noreply.github.com>
2020-02-15 09:30:38 -0800

    Added music folder as well as made a playerstate

---
+++ RhythmGame/Assets/Scripts/PlayerState.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    private int AmountCoins;
+
+    public enum Instrument
+    {
+        Lute,
+        Flute,
+        Bongos,
+        Trumpet,
+        None
+    }
+
+    private Instrument CurrentInstrument;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AmountCoins = 0;
+        CurrentInstrument = Instrument.None;
+    }
+
+    public int GetAmountCoins()
+    {
+        return AmountCoins;
+    }
+
+    public void SetAmountCoins(int NewAmountCoins)
+    {
+        AmountCoins = NewAmountCoins;
+    }
+
+    public void AddAmountCoins(int CoinsToAdd)
+    {
+        SetAmountCoins(AmountCoins + CoinsToAdd);
+    }
+
+    public void HalfCoinAmount()
+    {
+        SetAmountCoins(AmountCoins / 2);
+    }
+
+    public Instrument GetInstrument()
+    {
+        return CurrentInstrument;
+    }
+
+    public void SetInstrument(Instrument NewInstrument)
+    {
+        CurrentInstrument = NewInstrument;
+    }
+}
